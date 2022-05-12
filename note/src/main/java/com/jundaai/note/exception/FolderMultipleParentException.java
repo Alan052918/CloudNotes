8dc943d9f2d2d5b1c1ea3e31e8e6aa13728f5a0a
Alan052918 <aijunda29@gmail.com>
2022-05-12 02:29:59 -0400

    Remove redundant exceptions

--- note/src/main/java/com/jundaai/note/exception/FolderMultipleParentException.java
+++
@@ -1,9 +0,0 @@
-package com.jundaai.note.exception;
-
-public class FolderMultipleParentException extends RuntimeException {
-
-    public FolderMultipleParentException(Long folderId) {
-        super("Folder by id: " + folderId + " is not newly created and already has a parent.");
-    }
-
-}
--- note/src/main/java/com/jundaai/note/exception/NoteMultipleFolderException.java
+++
@@ -1,9 +0,0 @@
-package com.jundaai.note.exception;
-
-public class NoteMultipleFolderException extends RuntimeException {
-
-    public NoteMultipleFolderException(Long noteId) {
-        super("Note by id: " + noteId + " is not newly created and already has a folder.");
-    }
-
-}
