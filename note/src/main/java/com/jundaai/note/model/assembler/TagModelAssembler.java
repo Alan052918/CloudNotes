18db974d804831f35a0a1155dbb00228066cd197
Alan052918 <aijunda29@gmail.com>
2022-05-12 18:58:12 -0400

    Complete Tag model assembler

--- note/src/main/java/com/jundaai/note/model/assembler/TagModelAssembler.java
+++ note/src/main/java/com/jundaai/note/model/assembler/TagModelAssembler.java
@@ -1,17 +1,23 @@
 package com.jundaai.note.model.assembler;
 
+import com.jundaai.note.controller.TagController;
 import com.jundaai.note.model.Tag;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @Component
 public class TagModelAssembler implements RepresentationModelAssembler<Tag, EntityModel<Tag>> {
 
     @Override
     public EntityModel<Tag> toModel(Tag entity) {
-        return null;
+        return EntityModel.of(entity,
+                linkTo(methodOn(TagController.class).getTagById(entity.getId())).withSelfRel(),
+                linkTo(methodOn(TagController.class).getAllTags()).withRel("all tags"));
     }
 
     @Override
