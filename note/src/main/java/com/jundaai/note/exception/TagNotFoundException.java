e1e2ec179799dee94abe0b904acecea0315bfbab
Alan052918 <aijunda29@gmail.com>
2022-05-13 15:33:29 -0400

    Change TagNotFoundException message

--- note/src/main/java/com/jundaai/note/exception/TagNotFoundException.java
+++ note/src/main/java/com/jundaai/note/exception/TagNotFoundException.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class TagNotFoundException extends RuntimeException {
 
-    public TagNotFoundException(Long tagId, String tagName) {
-        super("Tag by id: " + tagId + " and name: " + tagName + " was not found.");
+    public TagNotFoundException(String idOrName) {
+        super("Tag by " + idOrName + " was not found.");
     }
 
 }
