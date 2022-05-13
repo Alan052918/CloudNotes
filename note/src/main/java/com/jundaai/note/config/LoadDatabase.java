d4b55914128533104f501d73f857ce473c18fd60
Alan052918 <aijunda29@gmail.com>
2022-05-12 20:18:05 -0400

    Add load database configuration

--- note/src/main/java/com/jundaai/note/config/LoadDatabase.java
+++ note/src/main/java/com/jundaai/note/config/LoadDatabase.java
@@ -1,7 +1,11 @@
 package com.jundaai.note.config;
 
 import com.jundaai.note.model.Folder;
+import com.jundaai.note.model.Note;
+import com.jundaai.note.model.Tag;
 import com.jundaai.note.repository.FolderRepository;
+import com.jundaai.note.repository.NoteRepository;
+import com.jundaai.note.repository.TagRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -9,16 +13,19 @@ import org.springframework.context.annotation.Configuration;
 
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @Slf4j
 public class LoadDatabase {
 
     @Bean
-    CommandLineRunner initDatabase(FolderRepository folderRepository) {
+    CommandLineRunner initDatabase(FolderRepository folderRepository,
+                                   NoteRepository noteRepository,
+                                   TagRepository tagRepository) {
         return args -> {
             ZonedDateTime now = ZonedDateTime.now();
-            folderRepository.save(Folder.builder()
+            Folder root = folderRepository.save(Folder.builder()
                     .name("root")
                     .createdAt(now)
                     .updatedAt(now)
@@ -26,6 +33,56 @@ public class LoadDatabase {
                     .subFolders(new ArrayList<>())
                     .notes(new ArrayList<>())
                     .build());
+            Folder java = folderRepository.save(Folder.builder()
+                    .name("Java")
+                    .createdAt(now)
+                    .updatedAt(now)
+                    .parentFolder(root)
+                    .subFolders(new ArrayList<>())
+                    .notes(new ArrayList<>())
+                    .build());
+            folderRepository.save(Folder.builder()
+                    .name("iOS")
+                    .createdAt(now)
+                    .updatedAt(now)
+                    .parentFolder(root)
+                    .subFolders(new ArrayList<>())
+                    .notes(new ArrayList<>())
+                    .build());
+            noteRepository.save(Note.builder()
+                    .name("Summary")
+                    .content("This is a cloud note storage.")
+                    .createdAt(now)
+                    .updatedAt(now)
+                    .folder(root)
+                    .tags(new ArrayList<>())
+                    .build());
+
+            Note jdbc = noteRepository.save(Note.builder()
+                    .name("JDBC")
+                    .content("Java Database Connection API.")
+                    .createdAt(now)
+                    .updatedAt(now)
+                    .folder(java)
+                    .tags(new ArrayList<>())
+                    .build());
+            Tag pl = tagRepository.save(Tag.builder()
+                    .name("Programming Language")
+                    .createdAt(now)
+                    .updatedAt(now)
+                    .notes(new ArrayList<>())
+                    .build());
+
+            List<Tag> tags = jdbc.getTags();
+            tags.add(pl);
+            jdbc.setTags(tags);
+
+            List<Note> notes = pl.getNotes();
+            notes.add(jdbc);
+            pl.setNotes(notes);
+
+            noteRepository.save(jdbc);
+            tagRepository.save(pl);
         };
     }
 
