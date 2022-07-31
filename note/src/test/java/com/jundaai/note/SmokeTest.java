46474adc0bc475c62d3289644f16e4079806658b
Junda Ai <39738304+Alan052918@users.noreply.github.com>
2022-07-31 15:47:55 -0400

    Add smoke test

---
+++ note/src/test/java/com/jundaai/note/SmokeTest.java
@@ -0,0 +1,58 @@
+package com.jundaai.note;
+
+import com.jundaai.note.controller.FolderController;
+import com.jundaai.note.controller.NoteController;
+import com.jundaai.note.controller.TagController;
+import com.jundaai.note.repository.FolderRepository;
+import com.jundaai.note.repository.NoteRepository;
+import com.jundaai.note.repository.TagRepository;
+import com.jundaai.note.service.FolderService;
+import com.jundaai.note.service.NoteService;
+import com.jundaai.note.service.TagService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class SmokeTest {
+
+    @Autowired
+    private FolderController folderController;
+    @Autowired
+    private NoteController noteController;
+    @Autowired
+    private TagController tagController;
+
+    @Autowired
+    private FolderService folderService;
+    @Autowired
+    private NoteService noteService;
+    @Autowired
+    private TagService tagService;
+
+    @Autowired
+    private FolderRepository folderRepository;
+    @Autowired
+    private NoteRepository noteRepository;
+    @Autowired
+    private TagRepository tagRepository;
+
+    @Test
+    public void contextLoads() {
+        assertNotNull(folderController);
+        assertNotNull(noteController);
+        assertNotNull(tagController);
+
+        assertNotNull(folderService);
+        assertNotNull(noteService);
+        assertNotNull(tagService);
+
+        assertNotNull(folderRepository);
+        assertNotNull(noteRepository);
+        assertNotNull(tagRepository);
+    }
+
+}
