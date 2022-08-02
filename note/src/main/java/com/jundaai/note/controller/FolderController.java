64759969c9f7e19fa00df13466816844791f8d4b
Junda Ai <39738304+Alan052918@users.noreply.github.com>
2022-08-02 03:37:36 -0400

    Return ResponseEntity in FolderController

--- note/src/main/java/com/jundaai/note/controller/FolderController.java
+++ note/src/main/java/com/jundaai/note/controller/FolderController.java
@@ -9,11 +9,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
 import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -31,45 +32,52 @@ public class FolderController {
     }
 
     @GetMapping
-    public CollectionModel<EntityModel<Folder>> getAllFolders() {
+    public ResponseEntity<CollectionModel<EntityModel<Folder>>> getAllFolders() {
         log.info("Request to get all folders");
-        List<Folder> folders = folderService.getAllFolders();
-        return folderModelAssembler.toCollectionModel(folders);
+        final List<Folder> folders = folderService.getAllFolders();
+        return ResponseEntity.ok(folderModelAssembler.toCollectionModel(folders));
     }
 
     @GetMapping(path = "{folderId}")
-    public EntityModel<Folder> getFolderById(@PathVariable(name = "folderId") Long folderId) {
+    public ResponseEntity<EntityModel<Folder>> getFolderById(@PathVariable(name = "folderId") Long folderId) {
         log.info("Request to get folder by id: {}", folderId);
-        Folder folder = folderService.getFolderById(folderId);
-        return folderModelAssembler.toModel(folder);
+        final Folder folder = folderService.getFolderById(folderId);
+        return ResponseEntity.ok(folderModelAssembler.toModel(folder));
     }
 
     @GetMapping(path = "{folderId}/subFolders")
-    public CollectionModel<EntityModel<Folder>> getSubFoldersByParentId(@PathVariable(name = "folderId") Long parentId) {
+    public ResponseEntity<CollectionModel<EntityModel<Folder>>> getSubFoldersByParentId(
+            @PathVariable(name = "folderId") Long parentId
+    ) {
         log.info("Request to get sub-folders by parent id: {}", parentId);
-        List<Folder> subFolders = folderService.getSubFoldersByParentId(parentId);
-        return folderModelAssembler.toCollectionModel(subFolders);
+        final List<Folder> subFolders = folderService.getSubFoldersByParentId(parentId);
+        return ResponseEntity.ok(folderModelAssembler.toCollectionModel(subFolders));
     }
 
     @PostMapping(path = "{folderId}/subFolders")
-    @ResponseStatus(HttpStatus.CREATED)
-    public EntityModel<Folder> createFolderByParentId(@PathVariable(name = "folderId") Long parentId,
-                                                      @Valid @RequestBody FolderCreationForm creationForm) {
+    public ResponseEntity<EntityModel<Folder>> createFolderByParentId(
+            @PathVariable(name = "folderId") Long parentId,
+            @Valid @RequestBody FolderCreationForm creationForm
+    ) {
         log.info("Request to create new folder: {}, parent folder id: {}", creationForm, parentId);
-        Folder folder = folderService.createFolderByParentId(parentId, creationForm);
-        return folderModelAssembler.toModel(folder);
+        final Folder folder = folderService.createFolderByParentId(parentId, creationForm);
+        final URI uri = MvcUriComponentsBuilder
+                .fromController(getClass())
+                .path("{folderId}/subFolders")
+                .buildAndExpand(folder.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(folderModelAssembler.toModel(folder));
     }
 
     @PatchMapping(path = "{folderId}")
-    public EntityModel<Folder> updateFolderById(@PathVariable(name = "folderId") Long folderId,
-                                                @Valid @RequestBody FolderUpdateForm updateForm) {
+    public ResponseEntity<EntityModel<Folder>> updateFolderById(@PathVariable(name = "folderId") Long folderId,
+                                                                @Valid @RequestBody FolderUpdateForm updateForm) {
         log.info("Request to update folder by id: {}, form: {}", folderId, updateForm);
-        Folder folder = folderService.updateFolderById(folderId, updateForm);
-        return folderModelAssembler.toModel(folder);
+        final Folder folder = folderService.updateFolderById(folderId, updateForm);
+        return ResponseEntity.ok(folderModelAssembler.toModel(folder));
     }
 
     @DeleteMapping("{folderId}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
     public ResponseEntity<?> deleteFolderById(@PathVariable(name = "folderId") Long folderId) {
         log.info("Request to delete folder by id: {}", folderId);
         folderService.deleteFolderById(folderId);
