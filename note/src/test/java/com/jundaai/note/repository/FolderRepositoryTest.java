75d4bc3966207fa7a0787c927d41f2f046f8e646
Junda Ai <39738304+Alan052918@users.noreply.github.com>
2022-08-02 03:19:01 -0400

    Add unit tests for FolderRepository

---
+++ note/src/test/java/com/jundaai/note/repository/FolderRepositoryTest.java
@@ -0,0 +1,87 @@
+package com.jundaai.note.repository;
+
+import com.jundaai.note.model.Folder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+public class FolderRepositoryTest extends RepositoryTest {
+
+    @Autowired
+    private FolderRepository testRepository;
+
+    @Override
+    @BeforeEach
+    void setUp() {
+        super.setUp();
+        testRepository.saveAll(mockFolders);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        testRepository.deleteAll();
+    }
+
+    @Test
+    public void findSubFoldersByParentId_Success() {
+        // given
+        Long testId = testRepository.findAll().get(0).getId();
+        List<String> expectedFolderNames = Arrays.asList(mockFolders.get(1).getName(), mockFolders.get(2).getName());
+
+        // when
+        List<Folder> gotFolders = testRepository.findSubFoldersByParentId(testId).orElse(null);
+
+        // then
+        assert gotFolders != null;
+        assertEquals(expectedFolderNames, gotFolders.stream().map(Folder::getName).toList());
+    }
+
+    @Test
+    public void findSubFoldersByParentId_NotExistingParentId_NullFound() {
+        // given
+        Long notExistingId = -1L;
+        testRepository.findAll().forEach(folder -> System.out.println(folder.getId()));
+
+        // when
+        List<Folder> gotFolders = testRepository.findSubFoldersByParentId(notExistingId).orElse(null);
+
+        // then
+        assertEquals(new ArrayList<>(), gotFolders);
+    }
+
+    @Test
+    public void existsByNameWithSameParent_True() {
+        // given
+        String testName = mockFolders.get(1).getName();
+        Folder testParent = mockFolders.get(0);
+
+        // when
+        boolean existsByNameWithSameParent = testRepository.existsByNameWithSameParent(testName, testParent);
+
+        // then
+        assertTrue(existsByNameWithSameParent);
+    }
+
+    @Test
+    public void existsByNameWithSameParent_False() {
+        // given
+        String testName = "New Folder";
+        Folder testParent = mockFolders.get(0);
+
+        // when
+        boolean existsByNameWithSameParent = testRepository.existsByNameWithSameParent(testName, testParent);
+
+        // then
+        assertFalse(existsByNameWithSameParent);
+    }
+
+}
---
+++ note/src/test/java/com/jundaai/note/repository/RepositoryTest.java
@@ -0,0 +1,93 @@
+package com.jundaai.note.repository;
+
+import com.jundaai.note.model.Folder;
+import com.jundaai.note.model.Note;
+import com.jundaai.note.model.Tag;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class RepositoryTest {
+
+    List<Folder> mockFolders;
+    List<Note> mockNotes;
+    List<Tag> mockTags;
+
+    @BeforeEach
+    void setUp() {
+        mockFolders = new ArrayList<>();
+        mockNotes = new ArrayList<>();
+        mockTags = new ArrayList<>();
+        loadFoldersNotesAndTags();
+    }
+
+    void loadFoldersNotesAndTags() {
+        ZonedDateTime now = ZonedDateTime.now();
+        Folder root = Folder
+                .builder()
+                .name("root")
+                .createdAt(now)
+                .updatedAt(now)
+                .parentFolder(null)
+                .subFolders(new ArrayList<>())
+                .notes(new ArrayList<>())
+                .build();
+        Folder pl = Folder
+                .builder()
+                .name("Programming Languages")
+                .createdAt(now)
+                .updatedAt(now)
+                .parentFolder(root)
+                .subFolders(new ArrayList<>())
+                .notes(new ArrayList<>())
+                .build();
+        Folder ds = Folder
+                .builder()
+                .name("Data Structures")
+                .createdAt(now)
+                .updatedAt(now)
+                .parentFolder(root)
+                .subFolders(new ArrayList<>())
+                .notes(new ArrayList<>())
+                .build();
+        Note go = Note
+                .builder()
+                .name("Go")
+                .content("Go is a general purpose programming language.")
+                .createdAt(now)
+                .updatedAt(now)
+                .folder(pl)
+                .tags(new ArrayList<>())
+                .build();
+        Tag google = Tag
+                .builder()
+                .name("Google")
+                .createdAt(now)
+                .updatedAt(now)
+                .notes(new ArrayList<>())
+                .build();
+        Tag microsoft = Tag
+                .builder()
+                .name("Microsoft")
+                .createdAt(now)
+                .updatedAt(now)
+                .notes(new ArrayList<>())
+                .build();
+        List<Tag> tags = go.getTags();
+        tags.add(google);
+        go.setTags(tags);
+        List<Note> notes = new ArrayList<>();
+        notes.add(go);
+        pl.setNotes(notes);
+        google.setNotes(notes);
+        mockFolders.add(root);
+        mockFolders.add(pl);
+        mockFolders.add(ds);
+        mockNotes.add(go);
+        mockTags.add(google);
+        mockTags.add(microsoft);
+    }
+
+}
