d0b128609c2f1c7ea94f6d5bf4e0d7597dd54c6c
Junda Ai <39738304+Alan052918@users.noreply.github.com>
2022-08-02 02:22:39 -0400

    Make ServiceTest abstract

--- note/src/test/java/com/jundaai/note/service/ServiceTest.java
+++ note/src/test/java/com/jundaai/note/service/ServiceTest.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class ServiceTest {
+public abstract class ServiceTest {
 
     AutoCloseable autoCloseable;
 
