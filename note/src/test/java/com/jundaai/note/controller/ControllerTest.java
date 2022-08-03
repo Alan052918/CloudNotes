ef673a7ae744d8f79b3fed6839c51f3e1785efe0
Junda Ai <39738304+Alan052918@users.noreply.github.com>
2022-08-03 01:39:15 -0400

    Add unit tests for FolderController

---
+++ note/src/test/java/com/jundaai/note/controller/ControllerTest.java
@@ -0,0 +1,155 @@
+package com.jundaai.note.controller;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.jundaai.note.model.Folder;
+import com.jundaai.note.model.Note;
+import com.jundaai.note.model.Tag;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@WebMvcTest
+@ExtendWith(MockitoExtension.class)
+public class ControllerTest {
+
+    MockMvc mockMvc;
+
+    ObjectMapper mapper;
+    ListAppender<ILoggingEvent> loggingEventListAppender;
+
+    List<Folder> mockFolders;
+    List<Note> mockNotes;
+    List<Tag> mockTags;
+    List<Long> mockFolderIds;
+    List<Long> mockNoteIds;
+    List<Long> mockTagIds;
+
+    static final String BASE_PATH = "http://localhost/api/v1";
+
+    @BeforeEach
+    void setUp() {
+        mapper = JsonMapper
+                .builder()
+                .addModule(new JavaTimeModule())
+                .build();
+        loggingEventListAppender = new ListAppender<>();
+        loggingEventListAppender.start();
+
+        mockFolders = new ArrayList<>();
+        mockNotes = new ArrayList<>();
+        mockTags = new ArrayList<>();
+        loadFoldersNotesAndTags();
+    }
+
+    void loadFoldersNotesAndTags() {
+        ZonedDateTime now = ZonedDateTime.now();
+        Folder root = Folder
+                .builder()
+                .id(0L)
+                .name("root")
+                .createdAt(now)
+                .updatedAt(now)
+                .parentFolder(null)
+                .subFolders(new ArrayList<>())
+                .notes(new ArrayList<>())
+                .build();
+        Folder pl = Folder
+                .builder()
+                .id(1L)
+                .name("Programming Languages")
+                .createdAt(now)
+                .updatedAt(now)
+                .parentFolder(root)
+                .subFolders(new ArrayList<>())
+                .notes(new ArrayList<>())
+                .build();
+        Folder ds = Folder
+                .builder()
+                .id(2L)
+                .name("Data Structures")
+                .createdAt(now)
+                .updatedAt(now)
+                .parentFolder(root)
+                .subFolders(new ArrayList<>())
+                .notes(new ArrayList<>())
+                .build();
+        Note go = Note
+                .builder()
+                .id(3L)
+                .name("Go")
+                .content("Go is a general purpose programming language.")
+                .createdAt(now)
+                .updatedAt(now)
+                .folder(pl)
+                .tags(new ArrayList<>())
+                .build();
+        Tag google = Tag
+                .builder()
+                .id(4L)
+                .name("Google")
+                .createdAt(now)
+                .updatedAt(now)
+                .notes(new ArrayList<>())
+                .build();
+        Tag microsoft = Tag
+                .builder()
+                .id(5L)
+                .name("Microsoft")
+                .createdAt(now)
+                .updatedAt(now)
+                .notes(new ArrayList<>())
+                .build();
+        List<Tag> tags = go.getTags();
+        tags.add(google);
+        go.setTags(tags);
+        List<Note> notes = new ArrayList<>();
+        notes.add(go);
+        pl.setNotes(notes);
+        google.setNotes(notes);
+        mockFolders.add(root);
+        mockFolders.add(pl);
+        mockFolders.add(ds);
+        mockNotes.add(go);
+        mockTags.add(google);
+        mockTags.add(microsoft);
+        mockFolderIds = mockFolders
+                .stream()
+                .map(Folder::getId)
+                .collect(Collectors.toList());
+        mockNoteIds = mockNotes
+                .stream()
+                .map(Note::getId)
+                .collect(Collectors.toList());
+        mockTagIds = mockTags
+                .stream()
+                .map(Tag::getId)
+                .collect(Collectors.toList());
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+
+    @Test
+    public void smokeTest() {
+        assertNotNull(mapper);
+        assertNotNull(loggingEventListAppender);
+    }
+
+}
---
+++ note/src/test/java/com/jundaai/note/controller/FolderControllerTest.java
@@ -0,0 +1,245 @@
+package com.jundaai.note.controller;
+
+import com.jundaai.note.form.folder.FolderCreationForm;
+import com.jundaai.note.form.folder.FolderUpdateForm;
+import com.jundaai.note.model.Folder;
+import com.jundaai.note.model.assembler.FolderModelAssembler;
+import com.jundaai.note.service.FolderService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@WebMvcTest(FolderController.class)
+@ExtendWith(SpringExtension.class)
+public class FolderControllerTest extends ControllerTest {
+
+    @MockBean
+    private FolderService mockFolderService;
+    @MockBean
+    private FolderModelAssembler mockFolderModelAssembler;
+
+    private static final String FOLDER_PATH = "/folders";
+
+    @Override
+    @BeforeEach
+    void setUp() {
+        super.setUp();
+        JacksonTester.initFields(this, mapper);
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(new FolderController(mockFolderService, mockFolderModelAssembler))
+                .build();
+    }
+
+    @Test
+    public void getAllFolders_200Ok() throws Exception {
+        // given
+        CollectionModel<EntityModel<Folder>> collectionModel = mockFolders
+                .stream()
+                .map(folder -> EntityModel.of(
+                        folder,
+                        linkTo(methodOn(FolderController.class).getFolderById(folder.getId())).withSelfRel()
+                ))
+                .collect(Collectors.collectingAndThen(Collectors.toList(), CollectionModel::of));
+
+        // when, then
+        when(mockFolderService.getAllFolders()).thenReturn(mockFolders);
+        when(mockFolderModelAssembler.toCollectionModel(mockFolders)).thenReturn(collectionModel);
+        mockMvc
+                .perform(get(BASE_PATH + FOLDER_PATH).contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.size()").value(2))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].id").value(0))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].name").value("root"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[1].id").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[1].name").value("Programming Languages"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[2].id").value(2))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[2].name").value("Data Structures"));
+
+        verify(mockFolderService).getAllFolders();
+        verify(mockFolderModelAssembler).toCollectionModel(mockFolders);
+    }
+
+    @Test
+    public void getFolderById_200Ok() throws Exception {
+        // given
+        Long testId = mockFolderIds.get(0);
+        Folder testFolder = mockFolders.get(0);
+        EntityModel<Folder> entityModel = EntityModel.of(
+                testFolder,
+                linkTo(methodOn(FolderController.class).getFolderById(testId)).withSelfRel()
+        );
+
+        // when, then
+        when(mockFolderService.getFolderById(testId)).thenReturn(testFolder);
+        when(mockFolderModelAssembler.toModel(testFolder)).thenReturn(entityModel);
+        mockMvc
+                .perform(get(BASE_PATH + FOLDER_PATH + "/" + testId).contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.size()").value(8))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(testId))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("root"));
+
+        verify(mockFolderService).getFolderById(testId);
+        verify(mockFolderModelAssembler).toModel(testFolder);
+    }
+
+    @Test
+    public void getSubFoldersByParentId_200Ok() throws Exception {
+        // given
+        Long testId = mockFolderIds.get(0);
+        List<Folder> testFolders = List.of(mockFolders.get(1), mockFolders.get(2));
+        CollectionModel<EntityModel<Folder>> collectionModel = testFolders
+                .stream()
+                .map(folder -> EntityModel.of(
+                        folder,
+                        linkTo(methodOn(FolderController.class).getFolderById(folder.getId())).withSelfRel()
+                ))
+                .collect(Collectors.collectingAndThen(Collectors.toList(), CollectionModel::of));
+
+        // when, then
+        when(mockFolderService.getSubFoldersByParentId(testId)).thenReturn(testFolders);
+        when(mockFolderModelAssembler.toCollectionModel(testFolders)).thenReturn(collectionModel);
+        mockMvc
+                .perform(get(BASE_PATH + FOLDER_PATH + "/" + testId + "/subFolders")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.size()").value(2))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].id").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].name").value("Programming Languages"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[1].id").value(2))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[1].name").value("Data Structures"));
+
+        verify(mockFolderService).getSubFoldersByParentId(testId);
+        verify(mockFolderModelAssembler).toCollectionModel(testFolders);
+    }
+
+    @Test
+    public void createFolderByParentId_201Created() throws Exception {
+        // given
+        Long testId = mockFolderIds.get(0);
+        Long newId = 100L;
+        ZonedDateTime now = ZonedDateTime.now();
+        Folder newFolder = Folder
+                .builder()
+                .id(newId)
+                .name("New Folder")
+                .createdAt(now)
+                .updatedAt(now)
+                .parentFolder(mockFolders.get(0))
+                .subFolders(new ArrayList<>())
+                .notes(new ArrayList<>())
+                .build();
+        EntityModel<Folder> entityModel = EntityModel.of(
+                newFolder,
+                linkTo(methodOn(FolderController.class).getFolderById(newId)).withSelfRel()
+        );
+        String requestBody = """
+                {
+                    "name": "New Folder"
+                }
+                """;
+
+        // when, then
+        when(mockFolderService.createFolderByParentId(eq(testId), any(FolderCreationForm.class))).thenReturn(newFolder);
+        when(mockFolderModelAssembler.toModel(newFolder)).thenReturn(entityModel);
+        mockMvc
+                .perform(MockMvcRequestBuilders
+                        .post(BASE_PATH + FOLDER_PATH + "/" + testId + "/subFolders")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.size()").value(8))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(newId))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("New Folder"));
+
+        verify(mockFolderService).createFolderByParentId(eq(testId), any(FolderCreationForm.class));
+        verify(mockFolderModelAssembler).toModel(newFolder);
+    }
+
+    @Test
+    public void updateFolderById_200Ok() throws Exception {
+        // given
+        Folder testFolder = mockFolders.get(0);
+
+        assertEquals("root", testFolder.getName());
+
+        Long testId = testFolder.getId();
+        ZonedDateTime now = ZonedDateTime.now();
+        testFolder.setName("New Name");
+        testFolder.setUpdatedAt(now);
+        EntityModel<Folder> entityModel = EntityModel.of(
+                testFolder,
+                linkTo(methodOn(FolderController.class).getFolderById(testId)).withSelfRel()
+        );
+        String requestBody = """
+                {
+                    "updateType": "RENAME_FOLDER",
+                    "newName": "New Name"
+                }
+                """;
+
+        // when, then
+        when(mockFolderService.updateFolderById(eq(testId), any(FolderUpdateForm.class))).thenReturn(testFolder);
+        when(mockFolderModelAssembler.toModel(testFolder)).thenReturn(entityModel);
+        mockMvc
+                .perform(MockMvcRequestBuilders
+                        .patch(BASE_PATH + FOLDER_PATH + "/" + testId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.size()").value(8))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(testId))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("New Name"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.updatedAt").value(now.toEpochSecond()));
+
+        verify(mockFolderService).updateFolderById(eq(testId), any(FolderUpdateForm.class));
+        verify(mockFolderModelAssembler).toModel(testFolder);
+    }
+
+    @Test
+    public void deleteFolderById_204NoContent() throws Exception {
+        // given
+        Long testId = mockFolderIds.get(1);
+
+        // when, then
+        mockMvc
+                .perform(MockMvcRequestBuilders
+                        .delete(BASE_PATH + FOLDER_PATH + "/" + testId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+
+        verify(mockFolderService).deleteFolderById(testId);
+    }
+
+}
