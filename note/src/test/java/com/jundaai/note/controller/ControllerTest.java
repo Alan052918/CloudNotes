eaa2a104b14ec46d15666b47ceaf7d994838dcf2
Junda Ai <39738304+Alan052918@users.noreply.github.com>
2022-08-03 23:33:50 -0400

    Remove unused import

--- note/src/test/java/com/jundaai/note/controller/ControllerTest.java
+++ note/src/test/java/com/jundaai/note/controller/ControllerTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.ZonedDateTime;
